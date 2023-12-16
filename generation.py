b3451cdf3aef54a753a15015365303757810b5e2
Caio Alves Caldeira <caio.caldeira@most.com.br>
2023-12-16 18:26:49 -0300

    Implement Generation constructor and eval method
    Co-authored-by: lorenzosc <lorenzosc@users.noreply.github.com>

--- generation.py
+++ generation.py
@@ -1,18 +1,34 @@
-import random
-from strategy import Strategy
+import numpy as np
+from strategy import Strategy, match
+
 
 class Generation:
-    names: list[str]
     population: list[Strategy]
 
-    def __init__ ():
+    def __init__ (self, distributions: np.ndarray[np.ndarray]) -> None:
+        self.population = [
+            Strategy(idx, np.sum(distributions[idx]), distributions[idx])
+            for idx in range(distributions.shape[0])
+        ]
+
+    def avaliate_population (self) -> None:
+        for strategy in self.population:
+            fitness = []
+
+            for opponent in self.population:
+                if strategy.idx == opponent.idx:
+                    continue
+
+                str_points = strategy.distribution[opponent.idx]
+                opp_points = opponent.distribution[strategy.idx]
+                fitness.append(str_points / (str_points + opp_points))
+
+            strategy.fitness = np.average(fitness)
+
+        self.population.sort(key=lambda x: x.fitness, reverse=True)
+
+    def preserve_population () -> None:
         pass
 
-    def avaliate_population ():
-        pass
-
-    def preserve_population ():
-        pass
-
-    def generate_new_population ():
+    def generate_new_population (self) -> None:
         pass
